94773e278ccfb172f712940dece453674b2defca
Putranugroho14 <putranugroho61@gmail.com>
2024-03-14 10:46:08 +0700

    Membuat prosedur insertionsort

--- insertionshort/insertionshort.cpp
+++ insertionshort/insertionshort.cpp
@@ -30,7 +30,31 @@ void input() {   // Procedure untuk input
 	}
 }
 
+void insertionsort() {    // Prosedure Insertionsort
 
+	int temp;             // Membuat variable data temporer atau penyimpanan sementara
+	int i, j;             // Membuat variable j sebagai penanda
+
+	for (i = 1; i <= n - 1; i++) {    // 1. Looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];                // 2. simpan nilai arr[i] ke variable sementara temp
+
+		j = i - 1;                   // 3. setting nilai j sama dengan i-1;
+
+		while (j >= 0 && arr[j] > temp) // 4. Looping while dimana nilai j lebih besar sama dengan 0 dan 
+		{                               // arr[j] lebih besar daripada temp 
+			arr[j + 1] = arr[j];        // 4a. Simpan arr[j] ke dalam variable arr[j+1]
+			j--;                        // 4b. Decrement nilai j by 1
+		}
+		arr[j + 1] = temp;              // 5. simpan nilai temp ke dalam arr[j+1]
+
+
+		cout << "\nPass " << i << ": ";  // Output ke layar
+		for (int k = 0; k < n; k++) {    // Looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";       // Output ke layar
+		}
+	}
+}
 int main()
 {
     
